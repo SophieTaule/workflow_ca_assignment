c2d0cc641ffcfeb02604c8b0115b74ca6ad39fe5
SophieTaule <49538115+SophieTaule@users.noreply.github.com>
2020-09-13 05:24:21 +0200

    initial commit

---
+++ app/scripts/script.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width = '100%';
+    document.getElementById('main').style.marginLeft = '100%';
+  }
+
+  function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+  }
